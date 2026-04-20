a464bfb99a0aa8d8e5f3ee86a558b0837cad7dd2
Akash Biswal <aakashbiswal@gmail.com>
2026-04-20 02:32:20 +0000

    feat: add health check

--- main.go
+++ main.go
@@ -4,9 +4,22 @@ import (
 	"net/http"
 )
 
+func healthzHandler(resp http.ResponseWriter, req *http.Request) {
+	resp.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	resp.WriteHeader(http.StatusOK)
+	resp.Write([]byte("OK"))
+}
+
 func main() {
 	s := http.NewServeMux()
-	s.Handle("/", http.FileServer(http.Dir(".")))
+
+	// s.Handle("/app/", http.FileServer(http.Dir(".")))
+	s.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	s.HandleFunc(
+		"/healthz",
+		healthzHandler,
+	)
+
 	server := http.Server{
 		Addr:    ":8080",
 		Handler: s,
